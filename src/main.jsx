c839cbe1bdf0923291fefdd45b2f7bcbf4162922
Michael Parmeley <parmeleym@gmail.com>
2023-08-03 20:16:30 -0700

    file cleanup - removed routes as not needed anymore

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store/store.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </Provider>,
 )
